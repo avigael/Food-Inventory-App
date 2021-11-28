8c8611ebe57d4f6e067f6380ead80d5620df9fa3
avigael <code@gael.cc>
2021-11-28 16:23:39 -0600

    Initial Commit

---
+++ Food Tracker/Food_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_TrackerApp.swift
+//  Food Tracker
+//
+//  Created by Gael G. on 11/28/21.
+//
+
+import SwiftUI
+
+@main
+struct Food_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
